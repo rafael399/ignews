c5094596a36c8352c3e310116ef7aee239384810
rafael399 <rafael399@gmail.com>
2021-06-19 17:09:52 -0300

    estrutura inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <h1 className={styles.title}>Hello World</h1>
+  )
+}
