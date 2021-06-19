ce769c4100f8b352b66fc84cdd1e9792b55e8844
rafael399 <rafael399@gmail.com>
2021-06-19 18:56:08 -0300

    Subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import avatar from '../../public/images/avatar.svg'
+import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
 
 export default function Home() {
@@ -22,6 +23,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <Image src={avatar} alt="Girl coding" />
